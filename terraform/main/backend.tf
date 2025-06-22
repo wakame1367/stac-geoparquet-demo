9125f0bf72fc64c859ee14ef829db23598ad4c24
Kota Yuhara <hotwater1367@gmail.com>
2025-06-22 09:14:50 +0900

    Fix region value in S3 backend configuration in backend.tf

--- terraform/main/backend.tf
+++ terraform/main/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "tf-state-40f33dd3"
     key    = "stac-geoparquet-demo/main/terraform.tfstate"
-    region = aws.apne1
+    region = "ap-northeast-1"
   }
 }
